a96aeb974f15a02b040f078576f4456e6a64057f
Wahab3917 <wahab3917@gmail.com>
2024-10-28 22:07:59 +0500

    Configure redux toolkit store

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
-import Auth from "./components/Auth"
+import { useState, useEffect } from "react"
+import { Outlet } from "react-router-dom"
+import { useDispatch } from "react-redux"
+import authService from "./appwrite/auth"
+import { login, logout } from "./store/authSlice"
+import { Header, Footer } from "./components"
 
 function App() {
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
+  useEffect(() => {
+    authService.getUser().then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+    .finally(() => {
+      setLoading(false)
+    })
+  }, [])
+
+  return !loading ? (
     <>
-      <Auth/>
+      <Header />
+      <main>
+        {/* <Outlet /> */} Hello
+      </main>
+      <Footer />
     </>
-  )
+  ) : null
 }
 
 export default App
